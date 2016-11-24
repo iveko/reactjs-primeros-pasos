21252f7d9c804704d6f464d9ae3a068116463285
iveko <github.z.ivek@xoxy.net>
2016-11-24 15:17:10 -0300

    presentacion dia uno

--- 1-hello-world/app/components/main.jsx
+++ 1-hello-world/app/components/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//usar https://babeljs.io/repl  para ver la transpilación a javascript
 class HelloWorld extends React.Component{
   render(){
     return <div>Hello world!</div>
   }
 }
 
+const HelloWorldF = function () {
+	return <div>Hola, Soy una componente funcional</div>
+}
+
 ReactDOM.render(
-  <HelloWorld />,
+  <HelloWorldF />,
   document.getElementById('container')
 );
