e808a6336eec2872f35e7475ec6f081ab7edd4ce
iveko <github.z.ivek@xoxy.net>
2016-11-29 15:17:12 -0300

    widget config

--- 3-states/webpack.config.js
+++ 3-states/webpack.config.js
@@ -6,6 +6,12 @@ module.exports = {
   },
   module: {
     loaders: [
+    { test: /\.css$/,  loader: "style-loader!css-loader" },
+  { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+  { test: /\.gif$/, loader: "url-loader?mimetype=image/png" },
+  { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
+  { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
+
       {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
